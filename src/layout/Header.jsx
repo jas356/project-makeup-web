da89ae1215bf9d93b7ee2f42c4d7c4f2fa44289e
Jasmine Henry <Jasminehenry356@gmail.com>
2023-05-18 17:59:00 -0400

    Added a parallax scroller

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,38 @@
+import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
+import { Container, Row, Col } from 'react-bootstrap';
+import Image from '../gallery/3models.jpg';
+import Image1 from "../gallery/22.webp"
+
 export default function Header() {
     return(
-        <header>
-            <h1>Understanding Skin Undertones</h1>
-        </header>
+        <Container>
+            <Row>
+                <Col>
+                    <>
+                    <ParallaxProvider>
+                    <header>
+                    <h1>Understanding Skin Undertones</h1>
+                    <Parallax speed={-10}
+                        translateX={['-400px', '0px']}
+                        scale={[0.75, 1]}
+                        rotate={[-180, 0]}
+                        easing="easeInQuad">
+                        <img src={Image} alt=''/>
+                    </Parallax>
+                    <Parallax speed={20}
+                    
+                        translateX={['-400px', '0px']}
+                        scale={[0.75, 1]}
+                        rotate={[-180, 0]}
+                        easing="easeInQuad">
+                        <img src={Image1} />
+                    </Parallax>
+
+                    </header>
+                    </ParallaxProvider>
+                    </>
+                </Col>
+            </Row>
+        </Container>
     )
 }
